235bb87cfb496d1e2d0f5cbb86d7dc5c80bd3cf7
Tukaram Garad <115521407+tukagarad07@users.noreply.github.com>
2024-01-25 11:38:40 +0000

    all basic design

---
+++ Timely/js/newSchedule.js
@@ -0,0 +1,9 @@
+// script.js
+
+function addNewTarget() {
+    // Assuming you want to show a simple alert as a popup
+    alert("Schedule is Updated");
+
+    // Redirect to index.html
+    window.location.href = "/Timely/index.html";
+}
